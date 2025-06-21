5c4dbafc2aa97cda58dfdd115e1f0f1007700c46
Michal Kuropieska <154534560+98Miquelle11@users.noreply.github.com>
2025-06-21 18:32:28 +0200

    Add files via upload

---
+++ C#-course-fast-start/3. Typy danych/Konwersja typów - rzutowanie/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Konwersja_typów___rzutowanie
+{
+    internal class Program // rzutowanie jawne i niejawne
+    {
+        static void Main(string[] args)
+        {
+            int varInt = 200000;
+            long varLong;
+
+            varLong = varInt; // zaszło niejawne rzutowanie. Intiger zmieści się w longu
+            Console.WriteLine(varLong);
+
+            short varShort;
+            //varShort = varInt; // nie można niejawnie przekonwertować int na short.
+            varShort = (short)varInt;
+            Console.WriteLine(varShort); // zapisało się tylko tyle bitów ile short posiada
+
+            float varFloat = 5.5f;
+            varLong = (long)varFloat;
+            Console.WriteLine(varLong); // pokaże tylko wartość całkowitą
+
+            varFloat = varInt;
+            Console.WriteLine(varFloat); // tutaj nie ma problemu bo float również zawiera liczby całkowite
+
+            string tekst = "123";
+            varInt = (int)(object)tekst;
+            Console.WriteLine(tekst);
+
+            Console.ReadKey();
+        }
+    }
+}
