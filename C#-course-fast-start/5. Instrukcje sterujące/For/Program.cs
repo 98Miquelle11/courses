da0247e43ee52819abd217b01e8ea864458356b7
Michal Kuropieska <154534560+98Miquelle11@users.noreply.github.com>
2025-06-22 21:45:01 +0200

    Add files via upload

---
+++ C#-course-fast-start/5. Instrukcje sterujące/For/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace For
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Podaj liczbę: ");
+            int a = int.Parse(Console.ReadLine());
+            int i = 1;
+
+            for ( ; i <= 10; i += 1) // 1sza część to start, druga to koniec, 3cia to proces
+            {
+
+                Console.WriteLine(i);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C#-course-fast-start/5. Instrukcje sterujące/If/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace If
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ile masz lat?");
+            int wiek = int.Parse(Console.ReadLine());
+            if (wiek >= 18)
+            {
+                Console.WriteLine("Zapraszamy!");
+                if (wiek >= 50) Console.WriteLine("Stary jesteś...");
+            }
+            if (wiek < 18) Console.WriteLine("Do widzenia.");
+            Console.ReadKey();
+        }
+    }
+}
