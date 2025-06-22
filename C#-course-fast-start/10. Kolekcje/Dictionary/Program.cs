4237b6e270f6a9497388b8bc037d8c883b7e291b
Michal Kuropieska <154534560+98Miquelle11@users.noreply.github.com>
2025-06-22 21:59:24 +0200

    Add files via upload

---
+++ C#-course-fast-start/10. Kolekcje/Dictionary/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> dictionary = new Dictionary<int, string>();
+            dictionary.Add(1, "Poniedziałek");
+            dictionary.Add(2, "Wtorek");
+            dictionary.Add(3, "Środa");
+            //dictionary.Add(3, "Środa"); nie mogą być dwa takie same indeksy
+            dictionary.Add(0, "Środa"); // dictionary sam z siebie się nie sortuje
+
+            Console.WriteLine(dictionary[1]);
+
+            foreach(var item in dictionary)
+            {
+                Console.WriteLine($"{item.Key} - {item.Value}");
+            }
+
+            SortedDictionary<int, string> sd = new SortedDictionary<int, string>();
+            sd.Add(5, "Piątek");
+            sd.Add(4, "Czwartek");
+            foreach (var item in sd)
+            {
+                Console.WriteLine($"{item.Key} - {item.Value}");
+            } // będzie posortowane
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C#-course-fast-start/10. Kolekcje/SortedList/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedList<int, string> sl = new SortedList<int, string>();
+            sl.Add(1, "Poniedziałek");
+            sl.Add(2, "Wtorek");
+            sl.Add(3, "Środa");
+            sl.Add(4, "Czwartek");
+            sl.Add(5, "Piątek");
+            //sl.Add(5, "Piątek"); // kolekcja napotka błąd, ponieważ znalazła klucz powtarzalny
+
+            //Console.WriteLine(sl[0]); // indek w SortedList == klucz. Nie ma takiego klucza
+            Console.WriteLine(sl[1]);
+            foreach (var item in sl)
+            {
+                Console.WriteLine(item); // to co przechowuje się w tej kolekcji to klucz i wartość. To ze względu na jego klasę
+            }
+            Console.WriteLine("\n");
+            foreach (KeyValuePair<int,string> item in sl)
+            {
+                Console.WriteLine(item.Key + " - " + item.Value); // trzeba rozdzielić
+            }
+            Console.WriteLine("-----------------");
+            Console.WriteLine(sl.Keys[0]); // pierwszy klucz, który występuje na naszej liście
+            Console.WriteLine(sl.Values[0]); // pierwsza wartość
+
+            foreach(var item in sl.Values)
+            {
+                Console.WriteLine(item); // można też zrobić z kluczami
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
