cf2be84a97ba7b1980921e0735108465f007fd17
Michal Kuropieska <154534560+98Miquelle11@users.noreply.github.com>
2025-06-22 21:52:09 +0200

    Add files via upload

---
+++ C#-course-fast-start/8. Programowanie obiektowe/Indeksery/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indeksery
+{
+    internal class Program
+    {
+        class Tekst
+        {
+            char[] tekst;
+            // Właściwość to skrzyżowanie pola i metody – wygląda jak pole, a zachowuje się jak metoda.Dostęp do właściwości
+            // odbywa się przy użyciu dokładnie takiej samej składni, jak w przypadku dostępu do pola.  Natomiast kompilator
+            // automatycznie zmienia tę składnię na wywołanie metod akcesorów odczytu i zapisu.
+            public char this[int n]
+            {
+                get { return tekst[n]; }
+                set { tekst[n] = value; } // dzięki temu powinniśmy uzyskać nową możliwość naszego indeksatora, czyli możliwość przypisywania wartości
+            }
+            // Blok "get" zawiera instrukcje wykonywane w przypadku odczytu właściwości
+            // Blok "set" zawiera instrukcje wykonywane w przypadku zapisu właściwości.
+            // Typ właściwości określa typ danych odczytywanych i zapisywanych za pomocą akcesorów get i set.
+            public Tekst(string txt) // tworzymy indekser
+            {
+                tekst = new char[txt.Length];
+                for (int i = 0; i < txt.Length; i++)
+                {
+                    tekst[i] = txt[i];
+                }
+            }
+            public char this [string n] // indeksatory można przeciążać. Ten indekser może być również typu char
+            {
+                get
+                {
+                    if (n.Equals("jeden"))
+                    {
+                        return tekst[0];
+                    }
+                    return tekst[tekst.Length - 1];
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            string text = "Witaj świecie!";
+            Console.WriteLine(text[1]); // wyświetli się druga litera bo string zachowuje się tak, jakby był tablicą
+            // Nie jest tablicą 1 : 1, ale typ string implementuje indeksery
+            Tekst tekst = new Tekst("Hello");
+            Console.WriteLine(tekst["jeden"]); // nie zauważymy żadnych zmian w tekście
+            tekst[1] = '3';
+            for (int i = 0; i <= 4; i++)
+            {
+                Console.Write(tekst[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C#-course-fast-start/8. Programowanie obiektowe/Interfejsy/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfejsy // są naturalnym rozszerzeniem klas abstrakcyjnych. Najczęściej są wykorzystywane do tego, żeby po nich mogły dziedziczyć
+{                    // klasy (po dowolnej ilości interfejsów)
+    abstract class Shape
+    {
+        public abstract void Draw();
+    }
+    class Rectangle : Shape, IArea
+    {
+        public float a;
+        public float b;
+        public Rectangle(float a, float b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+
+        public void Area()
+        {
+            Console.WriteLine($"Pole prostokąta to {Math.Round(a*b,2)}");
+        }
+
+        public override void Draw()
+        {
+            Console.WriteLine($"rostokąt: a = {a}, b = {b}");
+        }
+    }
+    class Circle : Shape, IArea
+    {
+        public Circle(float r)
+        {
+            this.r = r;
+        }
+        public float r;
+        public override void Draw()
+        {
+            Console.WriteLine($"Koło: r = {r}");
+        }
+
+        public void Area()
+        {
+            Console.WriteLine($"Pole koła to {Math.Round(Math.PI * r * r, 2)}");
+        }
+    }
+    interface IArea //domyślną cechą interfejsu jest publiczność
+    {
+        void Area(); // interface niejako sam w sobie jest już abstrakcyjny, dlatego przy metodach nie trzeba dodawać przedrostka "abstract"
+        //void Other()
+        //{
+        //    Console.WriteLine(); // nie działa, stara wersja C#??
+        //}
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rectangle = new Rectangle(10, 3);
+            rectangle.Draw();
+            Circle circle = new Circle(4);
+            circle.Draw();
+            rectangle.Area();
+            circle.Area();
+
+            Console.ReadKey();
+        }
+    }
+}
